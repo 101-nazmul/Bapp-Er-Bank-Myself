fcd76dffa9424b8ed5c7b4b18aafb2133bec0ede
unknown <sn.nazmul.101@gmail.com>
2022-12-17 13:29:39 +0600

    all commit of git

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.getElementById('logInBtn').addEventListener('click', function(){
     const inputEmail = document.getElementById('inputEmail').value;
     const inputPassword = document.getElementById('inputPassword').value;
     if(inputEmail === 'sn.nazmul.102@gmail.com' && inputPassword === '.NazMul.101') {
-        window.location.href = 'Main Bank/index.html';
+        window.location.href = 'main/index.html';
     } else {
         alert('Unvalid User')
     }
